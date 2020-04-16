77240c473eaa2e0c00547eff6d2d47fe25213626
luki981259 <59644074+luki981259@users.noreply.github.com>
2020-04-16 17:24:07 +0200

    dramat

--- MP3_PLAYER/Core/Src/main.c
+++ MP3_PLAYER/Core/Src/main.c
@@ -23,7 +23,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "string.h"
+#include <stdio.h>
 #include"ff.h"
 extern const uint8_t rawAudio[123200];
 
@@ -178,7 +179,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef*huart)
 			}
 	}
 
-
+/*
 void read_song(){
 
 	DIR dir;
@@ -189,6 +190,38 @@ void read_song(){
 	i = strlen(path);
 	sprintf(&path[i], "/%s", fno.fname);
 	printf("%s/%s\n", path, fno.fname);
+}
+*/
+
+FRESULT scan_files (
+    char* path        /* Start node to be scanned (***also used as work area***) */
+)
+{
+    FRESULT res;
+    DIR dir;
+    UINT i;
+    static FILINFO fno;
+
+
+    res = f_opendir(&dir, path);                       /* Open the directory */
+    if (res == FR_OK) {
+        for (;;) {
+            res = f_readdir(&dir, &fno);                   /* Read a directory item */
+            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
+            if (fno.fattrib & AM_DIR) {                    /* It is a directory */
+                i = strlen(path);
+                sprintf(&path[i], "/%s", fno.fname);
+                res = scan_files(path);                    /* Enter the directory */
+                if (res != FR_OK) break;
+                path[i] = 0;
+            } else {                                       /* It is a file. */
+                printf("%s/%s\n", path, fno.fname);
+            }
+        }
+        f_closedir(&dir)
+    }
+
+    return res;
 }
 
 /* USER CODE END PFP */
@@ -236,12 +269,23 @@ int main(void)
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 
-  fresult = f_mount(&FatFs, "", 0);
+  //fresult = f_mount(&FatFs, "", 0);
+
+  FATFS fs;
+     FRESULT res;
+     char buff[256];
+
+
+     res = f_mount(&fs, "", 1);
+     if (res == FR_OK) {
+         strcpy(buff, "/");
+         res = scan_files(buff);
+     }
 
 
   HAL_DAC_Start(&hdac,DAC_CHANNEL_1);
   HAL_ADC_Start_IT(&hadc1);
-  read_song();
+  //read_song();
   /* USER CODE END 2 */
 
   /* Infinite loop */
